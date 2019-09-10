80c54f33d398900f1397081beff3a9b54be35749
Simon <gerolddev@gmail.com>
2019-09-10 12:45:32 +0200

    Created filtering on Chapter in lost item table

--- src/lostitemtable.js
+++ src/lostitemtable.js
@@ -65,6 +65,18 @@ const tableColumns = [
     key: 'chapter',
     width: 150,
     sorter: (a, b) => a.chapter - b.chapter,
+    filters: [
+      {text: "3", value: "3"},
+      {text: "4", value: "4"},
+      {text: "5", value: "5"},
+      {text: "6", value: "6"},
+      {text: "7", value: "7"},
+      {text: "8", value: "8"},
+      {text: "9", value: "9"},
+      {text: "10", value: "10"},
+      {text: "11", value: "11"}
+    ],
+    onFilter: (value, record) => record.chapter.indexOf(value) === 0,
   },
   {
     title: 'Location',
